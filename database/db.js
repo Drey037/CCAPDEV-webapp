f30f0d13f4ffc871258b0581c553e7b30795ed3a
Drey037 <104416862+Drey037@users.noreply.github.com>
2022-06-28 01:21:52 +0800

    updated db file, added validators file

--- database/db.js
+++ database/db.js
@@ -2,7 +2,20 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const mongoose = require('mongoose');
+<<<<<<< Updated upstream
 const url = process.env.MONGODB_URI;
+=======
+const url = 'mongodb://127.0.0.1/ReelTalkDB';     // Will have to change this for when we use MongoDB Atlas
+
+const Comment = require('./models/Comment');
+const LoginInfo = require('./models/Login-Info');
+const Review = require('./models/Review');
+const ShowInfo = require('./models/Show-Info');
+const UserInfo = require('./models/User-Info');
+const Watchlist = require('./models/Watchlist');
+
+
+>>>>>>> Stashed changes
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true
@@ -13,6 +26,68 @@ const database = {
         mongoose.connect(url, options, function(error) {
             if(error) throw error;
             console.log('Connected to: ' + url);
+        });
+    },
+
+    insertOne: function(model, doc, callback) {
+        model.create(doc, function(error, result) {
+            if(error) return callback(false);
+            console.log('Added ' + result);
+            return callback(true);
+        });
+    },
+
+    insertMany: function(model, docs, callback) {
+        model.insertMany(docs, function(error, result) {
+            if(error) return callback(false);
+            console.log('Added ' + result);
+            return callback(true);
+        });
+    },
+
+    findOne: function(model, query, projection, callback) {
+        model.findOne(query, projection, function(error, result) {
+            if(error) return callback(false);
+            return callback(result);
+        });
+    },
+
+    findMany: function(model, query, projection, callback) {
+        model.find(query, projection, function(error, result) {
+            if(error) return callback(false);
+            return callback(result);
+        });
+    },
+
+    updateOne: function(model, filter, update, callback) {
+        model.updateOne(filter, update, function(error, result) {
+            if(error) return callback(false);
+            console.log('Document modified: ' + result.nModified);
+            return callback(true);
+        });
+    },
+
+    updateMany: function(model, filter, update, callback) {
+        model.updateMany(filter, update, function(error, result) {
+            if(error) return callback(false);
+            console.log('Documents modified: ' + result.nModified);
+            return callback(true);
+        });
+    },
+
+    deleteOne: function(model, conditions, callback) {
+        model.deleteOne(conditions, function (error, result) {
+            if(error) return callback(false);
+            console.log('Document deleted: ' + result.deletedCount);
+            return callback(true);
+        });
+    },
+
+    deleteMany: function(model, conditions, callback) {
+        model.deleteMany(conditions, function (error, result) {
+            if(error) return callback(false);
+            console.log('Document deleted: ' + result.deletedCount);
+            return callback(true);
         });
     }
 };
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const hbs = require('hbs');
 const routes = require('./routes/routes.js');
 const db = require('./database/db.js');
 
+//Imports from Security and Authentication
+const session = require('express-session');
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo')(session);
+
 const app = new express();
 
 app.use(express.json());
@@ -14,6 +19,26 @@ app.use(fileUpload());
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
+
+
+//Session server config
+app.use(session({
+    secret: 'sefg93bfg3ut',
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
+    resave: false,
+    saveUninitialized: true,
+    cookies: {secure: false, maxAge: 1000 * 60 * 60 * 24 * 7}
+}));
+
+app.use(flash());
+
+//Flash message
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
+
 
 db.connect();
 
