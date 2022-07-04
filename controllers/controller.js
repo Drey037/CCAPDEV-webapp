cdf40fece9c20f1406dde31d68854f1c2ac4456e
Drey037 <104416862+Drey037@users.noreply.github.com>
2022-07-04 17:09:24 +0800

    Can now render index page with cards

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,26 +2,66 @@
 const reviewModel = require('../database/Review');
 const db = require('../database/db');
 const User = require('../database/User-Info.js');
+const Show = require('../database/Show-Info');
 
 const controller = {
     getIndex: function(req, res) {
-        // db.findMany(reviewModel, {}, null, async function(result) {
-        //     var users = await result.populate("user");
-        //     var shows = await result.populate("show");
-        //     db.findOne(User, {"_id": req.session.user}, null, async function(data) {
-        //         var watchlist = null;
-        //         if(req.session.user != null) {
-        //             var populatedData = await data.populate("watchlists");
-        //             watchlist = populatedData.watchlists;
-        //         }
+        db.findMany(reviewModel, {}, null, function(result) {
+            var reviews = [];
 
-                
+            /*
+            for (let i = 0; i , result.length; i++) {
+                db.findOne(reviewModel, {"_id": result[i].id}, null, async function(reviewData) {
 
-        //         console.log("Logged in: " + req.session.username);
-        //         res.render('index', {watchlist: watchlist, username: req.session.username, profile_pic: req.session.profile_pic, review: result, show: shows.show, user: users.user} );
-        //     });
-        // })
-        res.render('index', {username: req.session.username, profile_pic: req.session.profile_pic});
+                    var populatedData = await data.populate("watchlists");
+                    watchlist = populatedData.watchlists;
+                    reviews.push(userData);
+                });
+            }
+            */
+            db.findOne(User, {"_id": req.session.user}, null, async function(data) {
+                var watchlist = null;
+                if(req.session.user != null) {
+                    var populatedData = await data.populate("watchlists");
+                    watchlist = populatedData.watchlists;
+                }
+
+                for (let i = 0; i < result.length; i++) {
+                    db.findOne(User, {"_id": result[i].user}, null, async function(userData) {
+                        if (userData != null) {
+                            var username = userData.username;
+                        }
+                        else {
+                            var username = "Deleted User";
+                        }
+    
+                        db.findOne(Show, {"_id": result[i].show}, null, async function(showData) {
+                            var title = showData.title;
+                            var genre = showData.genre;
+                            var poster = showData.image;
+
+                            var description = result[i].description;
+                            description = description.substring(0, 200) + "...View more";
+    
+                            reviews.push({
+                                title: title,
+                                genre: genre,
+                                poster: poster,
+                                description: description,
+                                username: username
+                            });
+
+                            if (i == result.length-1) {
+                                console.log("Number of reviews is " + reviews.length);
+                                console.log("Logged in: " + req.session.username);
+                                res.render('index', {watchlist: watchlist, username: req.session.username, profile_pic: req.session.profile_pic, review: reviews} );
+                                //res.render('index', {watchlist: watchlist, username: req.session.username, profile_pic: req.session.profile_pic, review: result} );
+                            }
+                        });
+                    });
+                }
+            });
+        })
         
     },
 
