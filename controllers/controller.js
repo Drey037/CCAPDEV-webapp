8171e02e2c8aa48f3aec2f4707b9e7cbfe4897aa
Lila Sicuan <viktoria.sicuan@gmail.com>
2022-09-28 08:50:52 +0800

    Refactored getIndex in controller AGAIN

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,71 +7,53 @@ const { ObjectId } = require('mongodb');
 
 const controller = {
     getIndex: async function(req, res) {
-        let indexData = {
-            username: req.session.user,
+        let response = {
+            username: '',
             profile_pic: req.session.profile_pic,
             reviews: [],
             watchlists: [],
         };
 
         // Gets all reviews
-        var raw_reviews = [];
-        await db.findMany(reviewModel, {}, null, function(result) {
-            raw_reviews = result;
-            var otherUserId = null;
-        });
+        var found_reviews = await reviewModel.find();
 
         // Gets logged in user
-        await db.findOne(User, {"_id": req.session.user}, null, async function(data) {
-            if(req.session.user != null) {
-                var populatedData = await data.populate("watchlists");
-                indexData.watchlists = populatedData.watchlists;
-            }
-        });
+        if(req.session.user != null) {
+            var found_user = await User.findById(req.session.user);
+            var populatedData = await found_user.populate("watchlists");
+            response.username = found_user.username;
+            response.watchlists = populatedData.watchlists;
+        }
 
         // Gets User data of each review
-        var username = '';
-        for (let i = 0; i < raw_reviews.length; i++) {
-            await db.findOne(User, {"_id": raw_reviews[i].user}, null, function(userData) {
-                var otherUserId = null;
-                if (userData != null) {
-                    username = userData.username;
-                    var userId = ObjectId(userData._id);
-                    if(req.session.user != userId)
-                        otherUserId = userId;
-                }
-                else {
-                    username = "Deleted User";
-                }
-
-            });
+        for (let i = 0; i < found_reviews.length; i++) {
+            var found_user = await User.findById(found_reviews[i].user);
+            var username = '';
+            var otherUserId = null;
+            if(!found_user) {
+                username = found_user.username;
+                otherUserId = ObjectId(found_user._id);
+            }
+            else
+                username = "Deleted User";
 
             // Gets Show data of each review
-            await db.findOne(Show, {"_id": raw_reviews[i].show}, null, async function(showData) {
-                var title = showData.title;
-                var genre = showData.genre;
-                var poster = showData.image;
-
-                var description = raw_reviews[i].description;
-                description = description.substring(0, 200) + "...View more";
-
-                indexData.reviews.push({
-                    title: title,
-                    genre: genre,
-                    poster: poster,
-                    description: description,
-                    username: username,
-                    reviewID: raw_reviews[i].id,
-                    showID: raw_reviews[i].show,
-                    otherUserId: otherUserId
-                });
-            });
-            
+            var found_show = await Show.findById(found_reviews[i].show);
+            response.reviews.push({
+                title: found_show.title,
+                genre: found_show.genre,
+                poster: found_show.image,
+                description: (found_reviews[i].description).substring(0, 200) + "...View more",
+                username: username,
+                reviewID: found_reviews[i]._id,
+                showID: found_reviews[i].show,
+                otherUserId: otherUserId
+            });            
         }
             
-        console.log("Number of reviews is " + raw_reviews.length);
+        console.log("Number of reviews is " + found_reviews.length);
         console.log("Logged in: " + req.session.username);
-        res.render('index', indexData);
+        res.render('index', response);
         
     },
 
